2be0455970bef9b34ff546dff62529d6cd95ad43
Alec Miller <alecmichaelmiller@gmail.com>
2023-09-30 21:34:34 -0700

    lint fixes

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,36 +1,36 @@
-import { AUTON_WINNER, AllianceScore, ELEVATION, MatchScore, ROBOT1_TIER } from "@18x18az/maestro-interfaces"
-import { calculateMatchScore } from "./index"
+import { AUTON_WINNER, AllianceScore, ELEVATION, MatchScore } from '@18x18az/maestro-interfaces'
+import { calculateMatchScore } from './index'
 
 describe('calculateMatchScore', () => {
-    it('should return 1 for both right now', () => {
-        const redInput: AllianceScore = {
-            goalTriballs: 0,
-            zoneTriballs: 0,
-            allianceTriballsInGoal: 0,
-            allianceTriballsInZone: 0,
-            robot1Tier: ELEVATION.NONE,
-            robot2Tier: ELEVATION.NONE,
-        }
-        const blueInput: AllianceScore = {
-            goalTriballs: 0,
-            zoneTriballs: 0,
-            allianceTriballsInGoal: 0,
-            allianceTriballsInZone: 0,
-            robot1Tier: ELEVATION.NONE,
-            robot2Tier: ELEVATION.NONE,
-        }
+  it('should return 1 for both right now', () => {
+    const redInput: AllianceScore = {
+      goalTriballs: 0,
+      zoneTriballs: 0,
+      allianceTriballsInGoal: 0,
+      allianceTriballsInZone: 0,
+      robot1Tier: ELEVATION.NONE,
+      robot2Tier: ELEVATION.NONE
+    }
+    const blueInput: AllianceScore = {
+      goalTriballs: 0,
+      zoneTriballs: 0,
+      allianceTriballsInGoal: 0,
+      allianceTriballsInZone: 0,
+      robot1Tier: ELEVATION.NONE,
+      robot2Tier: ELEVATION.NONE
+    }
 
-        const rawScore: MatchScore = {
-            redScore: redInput,
-            blueScore: blueInput,
-            autonWinner: AUTON_WINNER.NONE
-        }
+    const rawScore: MatchScore = {
+      redScore: redInput,
+      blueScore: blueInput,
+      autonWinner: AUTON_WINNER.NONE
+    }
 
-        const scores = calculateMatchScore(rawScore)
+    const scores = calculateMatchScore(rawScore)
 
-        expect(scores.redScore).toBe(1)
-        expect(scores.blueScore).toBe(1)
-    })
+    expect(scores.redScore).toBe(1)
+    expect(scores.blueScore).toBe(1)
+  })
 })
 
 describe('calculateSkillsScore', () => {
